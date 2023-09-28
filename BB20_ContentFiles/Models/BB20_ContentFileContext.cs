ba4baea36f2fe896d51f286ec9dd1c829282f387
RoyMorillo <rmorillo@arkusnexus.com>
2023-09-27 20:03:45 -0400

    Creating models

---
+++ BB20_ContentFiles/Models/BB20_ContentFileContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BB20_ContentFiles.Models
+{
+    public partial class BB20_ContentFileContext : DbContext
+    {
+        public BB20_ContentFileContext()
+        {
+        }
+
+        public BB20_ContentFileContext(DbContextOptions<BB20_ContentFileContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ContentFile> ContentFiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContentFile>(entity =>
+            {
+                entity.ToTable("ContentFile");
+
+                entity.HasIndex(e => e.ContentFileId, "IX_ContentFile");
+
+                entity.HasIndex(e => e.ContentId, "IX_ContentID");
+
+                entity.Property(e => e.ContentFileId)
+                    .HasColumnName("ContentFileID")
+                    .HasComment("ID of the table");
+
+                entity.Property(e => e.AssociatedFileTitle)
+                    .HasMaxLength(75)
+                    .HasComment("Title of Content files");
+
+                entity.Property(e => e.AssociatedFiles).HasComment("es tipo archivo .zip, pdf, doc");
+
+                entity.Property(e => e.ContentId)
+                    .HasColumnName("ContentID")
+                    .HasComment("ID of the content Table");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("DateTime of the creation of the row");
+
+                entity.Property(e => e.DeleteFlag).HasComment("If the row is logicaly deleted (0 = false and 1 = true)");
+
+                entity.Property(e => e.RowVersion)
+                    .IsRowVersion()
+                    .IsConcurrencyToken()
+                    .HasComment("This field is for the management of the concurrency");
+
+                entity.Property(e => e.ShowTerms).HasComment("if the terms are displayed");
+
+                entity.Property(e => e.UpdatedDate)
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("DateTime of the update of the row");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BB20_ContentFiles/Models/ContentFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BB20_ContentFiles.Models
+{
+    public partial class ContentFile
+    {
+        /// <summary>
+        /// ID of the table
+        /// </summary>
+        public int ContentFileId { get; set; }
+        /// <summary>
+        /// ID of the content Table
+        /// </summary>
+        public int ContentId { get; set; }
+        /// <summary>
+        /// es tipo archivo .zip, pdf, doc
+        /// </summary>
+        public string AssociatedFiles { get; set; }
+        /// <summary>
+        /// if the terms are displayed
+        /// </summary>
+        public bool ShowTerms { get; set; }
+        /// <summary>
+        /// Title of Content files
+        /// </summary>
+        public string AssociatedFileTitle { get; set; }
+        /// <summary>
+        /// DateTime of the creation of the row
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// DateTime of the update of the row
+        /// </summary>
+        public DateTime UpdatedDate { get; set; }
+        /// <summary>
+        /// If the row is logicaly deleted (0 = false and 1 = true)
+        /// </summary>
+        public bool DeleteFlag { get; set; }
+        /// <summary>
+        /// This field is for the management of the concurrency
+        /// </summary>
+        public byte[] RowVersion { get; set; }
+    }
+}
