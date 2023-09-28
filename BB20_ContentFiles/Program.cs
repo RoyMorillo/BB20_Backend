13adb0265f0697886a53d220ccfb7b890093ee89
RoyMorillo <rmorillo@arkusnexus.com>
2023-09-27 20:35:17 -0400

    CRUD Created

--- BB20_ContentFiles/Program.cs
+++ BB20_ContentFiles/Program.cs
@@ -1,20 +1,89 @@
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using BB20_ContentFiles.Models;
+using BB20_Categories;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using BB20_ContentFiles.Repository.Services;
+using BB20_ContentFiles.Repository.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BB20_ContentFileContext>(options => options.UseSqlServer(connectionString));
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Todos", builder => builder.WithOrigins("*").WithHeaders("*").WithMethods("*"));
+});
+
+// Mapping
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Dependency Injection
+builder.Services.AddScoped<IContentFileRepository, ContentFileRepository>();
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Register the Swagger generator, defining 1 or more Swagger documents
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "BB20_ContentFiles", Version = "v1" });
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    c.EnableAnnotations();
+    c.IncludeXmlComments(XmlCommentsFilePath);
+});
+
+// Versioning
+builder.Services.AddApiVersioning(opt =>
+{
+    opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                    new HeaderApiVersionReader("x-api-version"),
+                                                    new MediaTypeApiVersionReader("x-api-version"));
+});
+
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Versioning
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+app.UseDeveloperExceptionPage();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    foreach (var description in provider.ApiVersionDescriptions)
+    {
+        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+            description.GroupName.ToUpperInvariant());
+    }
+});
+
+// global cors policy
+app.UseCors(x => x
+    .SetIsOriginAllowed(origin => true)
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
 
 app.UseHttpsRedirection();
 
---
+++ BB20_ContentFiles/Repository/Services/ContentFileRepository.cs
@@ -0,0 +1,117 @@
+﻿using AutoMapper;
+using BB20_ContentFiles.Models;
+using BB20_ContentFiles.Models.DTOs;
+using BB20_ContentFiles.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BB20_ContentFiles.Repository.Services;
+
+public class ContentFileRepository : IContentFileRepository
+{
+    private readonly BB20_ContentFileContext _context;
+    private readonly IMapper _mapper;
+
+    public ContentFileRepository(BB20_ContentFileContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<ContentFileDTO>> GetAll()
+    {
+        List<ContentFile> contentFiles = await _context.ContentFiles
+                                    .Where(x => x.DeleteFlag == false)
+                                    .AsNoTracking()
+                                    .ToListAsync();
+
+        return _mapper.Map<List<ContentFileDTO>>(contentFiles);
+    }
+
+    public async Task<ContentFileDTO> GetDataById(int contentFileId)
+    {
+        ContentFile contentFile = await _context.ContentFiles
+                                    .Where(x => x.DeleteFlag == false && x.ContentFileId == contentFileId)
+                                    .AsNoTracking()
+                                    .FirstOrDefaultAsync();
+
+        return _mapper.Map<ContentFileDTO>(contentFile);
+    }
+
+    public async Task<List<ContentFileDTO>> GetAllByContentId(int contentId)
+    {
+        List<ContentFile> contentFiles = await _context.ContentFiles
+                                    .Where(x => x.DeleteFlag == false && x.ContentId == contentId)
+                                    .AsNoTracking()
+                                    .ToListAsync();
+
+        return _mapper.Map<List<ContentFileDTO>>(contentFiles);
+    }
+
+    public Task<int> AddAsync(ContentFileDTO entity)
+    {
+        try
+        {
+            ContentFile contentFile = _mapper.Map<ContentFileDTO, ContentFile>(entity);
+
+            contentFile.DeleteFlag = false;
+            contentFile.CreatedDate = DateTime.Now;
+            contentFile.UpdatedDate = DateTime.Now;
+
+            _context.ContentFiles.Add(contentFile);
+            _context.SaveChanges();
+
+            return Task.FromResult(contentFile.ContentFileId);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<bool> UpdateAsync(ContentFileDTO entity)
+    {
+        try
+        {
+            ContentFile contentFile = _mapper.Map<ContentFileDTO, ContentFile>(entity);
+
+            contentFile.UpdatedDate = DateTime.Now;
+            contentFile.DeleteFlag = false;
+
+            _context.ContentFiles.Update(contentFile);
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<bool> RemoveAsync(int contentFileId)
+    {
+        try
+        {
+            ContentFile contentFile = _context.ContentFiles
+                                .Where(x => x.ContentFileId == contentFileId)
+                                .FirstOrDefault();
+
+            if (contentFile == null)
+            {
+                return false;
+            }
+
+            contentFile.UpdatedDate = DateTime.Now;
+            contentFile.DeleteFlag = true;
+
+            _context.ContentFiles.Update(contentFile);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
