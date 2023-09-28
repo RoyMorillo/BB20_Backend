d670a4dbc62e5ff22e11ba1642089792e92aec70
RoyMorillo <rmorillo@arkusnexus.com>
2023-09-28 12:52:49 -0400

    Fix Interior Category

--- BB20_InteriorCategory/Models/DTOs/InteriorCategoryDTO.cs
+++ BB20_InteriorCategory/Models/DTOs/InteriorCategoryDTO.cs
@@ -51,4 +51,20 @@ public class InteriorCategoryDTO
     /// Landing page SEO Description Metadata
     /// </summary>
     public string? SeodescMetadata { get; set; }
+    /// <summary>
+    /// DateTime of the creation of the row
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+    /// <summary>
+    /// DateTime of the update of the row
+    /// </summary>
+    public DateTime UpdatedDate { get; set; }
+    /// <summary>
+    /// If the row is logicaly deleted (0 = false and 1 = true)
+    /// </summary>
+    public bool DeleteFlag { get; set; }
+    /// <summary>
+    /// This field is for the management of the concurrency
+    /// </summary>
+    public byte[]? RowVersion { get; set; }
 }
